db75470a88da99b3bcb3c52b8f97476c39990b9c
Marat Khasanov <maratk@hailo.ai>
2024-10-30 14:43:33 +0100

    Change wdyr settings

--- src/wdyr.ts
+++ src/wdyr.ts
@@ -7,7 +7,7 @@ if (process.env.NODE_ENV === 'development') {
   whyDidYouRender(React, {
     trackAllPureComponents: true,
     logOnDifferentValues: true,
-    collapseGroups: true,
+    collapseGroups: false,
     trackHooks: true,
   });
 }
